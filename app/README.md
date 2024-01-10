ac3fa1c5442d1009fb17d608e12ccefa33c1cf24
Adam Collins <adam.finn.collins@gmail.com>
2024-01-11 00:34:35 +1300

    Init Next.js

---
+++ app/README.md
@@ -0,0 +1,5 @@
+### Resimate web app
+
+This is the web app of Resimate.
+
+React
---
+++ app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        Resimate App
+      </div>
+    </main>
+  )
+}
