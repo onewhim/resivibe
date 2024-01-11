843993131dad3ebeabf54cbd55cab07eb8ba6e32
Henry Anderson <henry.leo.anderson@gmail.com>
2024-01-11 21:44:48 +1300

    Add toast

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "Resimate",
@@ -21,7 +22,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <main>{children}</main>
+        <Toaster />
+      </body>
     </html>
   );
 }
